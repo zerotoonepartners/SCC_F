e59cd23be3443f2dc6aa625d6e0908bc3168963e
JJH <naturalflavor1007@naver.com>
2021-07-20 16:25:18 +0900

    [remote btn] modify remote name

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -2,10 +2,34 @@ import react, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
-import { ISO9001,ISO13148,ISO14001,ISO22000,FSSC22000,ISO22716,ISO45001 } from '../components/detailComp/iso';
-import {성과공유기업,여성기업,메인비즈,벤처기업,뿌리기술전문기업} from '../components/detailComp/business';
-import {가족친화인증,병역특례업체,노사문화우수기업,청년친화강소기업,인재육성형중소기업,공동근로복지기금설립} from "../components/detailComp/welfare";
-import {기업부설연구소, 기업부설창작연구소} from "../components/detailComp/lab";
+import {
+  ISO9001,
+  ISO13148,
+  ISO14001,
+  ISO22000,
+  FSSC22000,
+  ISO22716,
+  ISO45001,
+} from '../components/detailComp/iso';
+import {
+  성과공유기업,
+  여성기업,
+  메인비즈,
+  벤처기업,
+  뿌리기술전문기업,
+} from '../components/detailComp/business';
+import {
+  가족친화인증,
+  병역특례업체,
+  노사문화우수기업,
+  청년친화강소기업,
+  인재육성형중소기업,
+  공동근로복지기금설립,
+} from '../components/detailComp/welfare';
+import {
+  기업부설연구소,
+  기업부설창작연구소,
+} from '../components/detailComp/lab';
 import DetailSecOne from '../components/detailComp/detailsecone';
 import styled, { css, keyframes } from 'styled-components';
 import Remote from '../components/detailComp/remote';
@@ -21,7 +45,7 @@ function Detail() {
     'ISO45001',
   ];
   const labMenu = [
-    `기업부설연구소/기업전담부서`,
+    `기업부설연구소/연구개발전담부서`,
     '기업창작연구소/창작전담부서',
   ];
   const businMenu = [
@@ -95,31 +119,55 @@ function Detail() {
         {}
         {detNum === '00' ? (
           <>
-            <div id="ISO9001"><ISO9001 /></div>
-            <div id="ISO13148"><ISO13148 /></div>
-            <div id="ISO14001"><ISO14001 /></div>
-            <div id="ISO22000"><ISO22000 /></div>
-            <div id="FSSC22000"><FSSC22000 /></div>
-            <div id="ISO22716"><ISO22716 /></div>
-            <div id="ISO45001"><ISO45001 /></div>
+            <div id="ISO9001">
+              <ISO9001 />
+            </div>
+            <div id="ISO13148">
+              <ISO13148 />
+            </div>
+            <div id="ISO14001">
+              <ISO14001 />
+            </div>
+            <div id="ISO22000">
+              <ISO22000 />
+            </div>
+            <div id="FSSC22000">
+              <FSSC22000 />
+            </div>
+            <div id="ISO22716">
+              <ISO22716 />
+            </div>
+            <div id="ISO45001">
+              <ISO45001 />
+            </div>
             <Remote remoteMenu={remoteMenu} />
           </>
         ) : null}
         {detNum === '01' ? (
           <>
-            <기업부설연구소 /><기업부설창작연구소/>
+            <기업부설연구소 />
+            <기업부설창작연구소 />
             <Remote remoteMenu={remoteMenu} />
           </>
         ) : null}
         {detNum === '10' ? (
           <>
-            <성과공유기업/><여성기업/><메인비즈/><벤처기업/><뿌리기술전문기업/>
+            <성과공유기업 />
+            <여성기업 />
+            <메인비즈 />
+            <벤처기업 />
+            <뿌리기술전문기업 />
             <Remote remoteMenu={remoteMenu} />
           </>
         ) : null}
         {detNum === '11' ? (
           <>
-            <가족친화인증/><병역특례업체/><노사문화우수기업/><청년친화강소기업/><인재육성형중소기업/><공동근로복지기금설립/>
+            <가족친화인증 />
+            <병역특례업체 />
+            <노사문화우수기업 />
+            <청년친화강소기업 />
+            <인재육성형중소기업 />
+            <공동근로복지기금설립 />
             <Remote remoteMenu={remoteMenu} />
           </>
         ) : null}
