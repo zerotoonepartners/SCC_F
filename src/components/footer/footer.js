a6a0f8b3246cd305ffb50df651e1ba432e297b70
ChoiHeekyeong <gmlrud9701@naver.com>
2021-07-23 17:21:51 +0900

    INIT

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -37,10 +37,10 @@ function Footer() {
       </div>
       <div className="footerCopy">
         <span>
-          &copy; 2021 스타트업인증센터. All Rights Reserved.
           <span className="businCopy">
-            사업자등록번호: 13998 대표자: 김경태
-          </span>
+              사업자등록번호: 288-87-00902 대표자: 김경태
+            </span>
+          &copy; 2021 스타트업인증센터. All Rights Reserved.
         </span>
       </div>
     </>
