30e87818caf16a8707e32a466b9be2bbf07690eb
JJH <naturalflavor1007@naver.com>
2021-07-23 11:42:33 +0900

    form bari

--- src/pages/barifurcation.js
+++ src/pages/barifurcation.js
@@ -5,6 +5,8 @@ import './scss/barifurcation.scss';
 import Kakao from '../components/kakao/kakao';
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
+import p3 from '../../src/static/images/p3.png';
+import p6 from '../../src/static/images/p6.png';
 
 function Barifurcation() {
   return (
@@ -12,15 +14,37 @@ function Barifurcation() {
       <div className="bariWrapper">
         <Header />
         <div className="bariBody">
-          <NavLink to="/eduform" className="navlink">
-            <div className="twopill">교육 신청</div>
-          </NavLink>
-          <NavLink to="/form" className="navlink">
-            <div className="twopill">인증 신청</div>
-          </NavLink>
+          <div className="btnWrapper">
+            <div className="btnItemWrapper">
+              <div className="topDesc">
+                <span>
+                  스타트업을 위한 <br /> 퀄리티 있는 교육
+                </span>
+              </div>
+              <div className="imgBox">
+                <img src={p3} alt="" />
+              </div>
+              <NavLink to="/eduform" className="navlink">
+                <div className="twopill">교육 신청</div>
+              </NavLink>
+            </div>
+            <div className="breakLine"></div>
+            <div className="btnItemWrapper">
+              <div className="topDesc">
+                <span>
+                  기업의 가치를 <br /> 높이는 방법
+                </span>
+              </div>
+              <div className="imgBox">
+                <img src={p6} alt="" />
+              </div>
+              <NavLink to="/form" className="navlink">
+                <div className="twopill">인증 신청</div>
+              </NavLink>
+            </div>
+          </div>
           <Kakao />
         </div>
-        <Footer />
       </div>
     </>
   );
