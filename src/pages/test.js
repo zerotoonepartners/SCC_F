1e95aa7c26b13dc931822528835bda3b436482e3
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-02 15:05:11 +0900

    INIT

--- src/pages/test.js
+++ src/pages/test.js
@@ -8,12 +8,12 @@ function Test() {
     <div className="testWrapper">
         <div className="picAndContents">
             <div className="pictureWrapper">
-                <img src="https://img.hankyung.com/photo/202106/BF.26713751.1.jpg" alt="" />
+                <img src="https://lh3.googleusercontent.com/proxy/LSgsqaUYZMUqImrFd-uySO82tXs5VgWvyjQAkKn4ZDVDgLtLewB9ddlZt9rlJw_2vTOxqRVVab4YCnUQ8pW_SDhqzn8pErQMHoJ3qB2V9uH_MBQaW1b48EtBDqyG" alt="" />
             </div>
             <div className="contentsWrapper">
                 <div className="name">
-                    <span className="nameKor">이준호</span>
-                    <span className="nameEng">Junho Lee</span>
+                    <span className="nameKor">김선호</span>
+                    <span className="nameEng">Sunho Kim</span>
                 </div>
                 <div className="department">
                     <span>경영기획본부 | 심사역</span>
@@ -22,7 +22,13 @@ function Test() {
                     <span className="hashTag">#열정 #전문성 #굿리스너</span>
                     <span className="introduce">안녕하세요<br/>자기소개가 들어갈 부분입니다.<br/>
                     각자 자신을 소개할 내용을 적어주시면 됩니다.<br/>
-                    @@@@@@@@@@@@@@ 적어주세요.</span>
+                    자기소개가 들어갈 부분입니다.<br/>
+                    각자 자신을 소개할 내용을 적어주시면 됩니다.<br/>
+                    자기소개가 들어갈 부분입니다.<br/>
+                    각자 자신을 소개할 내용을 적어주시면 됩니다.<br/>
+                    자기소개가 들어갈 부분입니다.<br/>
+                    각자 자신을 소개할 내용을 적어주시면 됩니다.<br/>
+                    이미지 부분은 사진 크기에 따라 사이즈를 재조절 할 것 같습니다.<br/></span>
                 </div>
             </div>
 
