65fe056ca38c3869c985b469e04b5640c86f0512
ahn kyung rok <akrnote97@gmail.com>
2021-08-05 09:22:05 +0900

    url

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import { setEvent } from '../context/scc';
 
 export const getData = async () => {
   try {
-    const res = await axios.get(`${url}`); 
+    const res = await axios.get(`${url}`);
     return res.data;
   } catch (e) {
     console.log(e);
--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
 //export const url = 'http://15.164.57.120/event';
-export const url = 'https://www.ztopserver.com/event';
+export const url = 'https://ztopserver.com/event';
