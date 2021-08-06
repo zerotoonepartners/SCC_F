92b4d7f0ec14703f41d6ed01739d06f1108caf09
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-06 18:05:32 +0900

    vvv

---
+++ src/components/section/consultsection/consultSecSix/consultSecSix.js
@@ -0,0 +1,49 @@
+import './consultSecSix.scss'
+import cooIcon5 from '../../../../static/images/cooIcon5.png';
+
+
+function ConsultSecSix() {
+    return (
+      <>
+        <div className="consultSecSixWrapper">
+          <div className="applyBox">
+            <div className="applyTitleWrapper">
+              <div className="title">
+                <span>상담 신청</span>
+              </div>
+              <div className="subTitle">
+                <span>컨설팅 상담을 위해 아래 서류가 필요합니다.<br/>
+                상담을 원하시면 기업검토 문의를 눌러주세요.</span>
+              </div>
+            </div>
+
+            <div className="applyContentsWrapper">
+              <div className="iconWrapper">
+               <img src={cooIcon5} className="cooIcon" />
+              </div>
+              <div className="sentenceWrapper">
+                <div className="sentence">
+                  <span>1. 명함 사진</span>
+                  <span>2. 사업자등록증</span>
+                  <span>3. 회사소개서/IR자료</span>
+                  <span>4. 4대보험 가입자 명부</span>
+                </div>
+              </div>
+            </div>
+            
+            <div className="buttonWrapper">
+              <div className="applyButton">
+                <span>기업검토 문의</span>
+              </div>
+
+            </div>
+
+          </div>
+
+
+        </div>
+      </>
+    );
+  }
+  
+  export default ConsultSecSix;
